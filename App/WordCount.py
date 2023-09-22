b1ea00feb99335da288df7bd997f93432f053250
Andres <40176339+Andres20-Utec@users.noreply.github.com>
2023-09-22 18:10:38 -0500

    Ajuste

--- App/WordCount.py
+++ App/WordCount.py
@@ -1,6 +1,7 @@
 from pyspark import SparkConf, SparkContext
 from pyspark.sql import HiveContext
-from pyspark.sql.functions import explode, split, desc
+from pyspark.sql.functions import split
+from pyspark.sql.functions import desc
 
 # Configurar Spark
 conf = SparkConf().setAppName("PythonWordCount")
@@ -11,7 +12,8 @@ hiveContext = HiveContext(sc)
 data = hiveContext.read.format('com.databricks.spark.csv').options(header='false', inferSchema='true').load('movies.csv')
 
 # Realizar las transformaciones
-df = data.select(explode(split(data['_c0'], ' ')).alias('word')) \
+df = data.select(split(data['_c0'], ' ').alias('words')) \
+         .selectExpr('explode(words) as word') \
          .groupBy('word') \
          .count() \
          .orderBy(desc('count'))
