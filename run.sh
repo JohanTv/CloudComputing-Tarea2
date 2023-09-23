a7da56adbc4917ea8e0104939331149bb4228832
Andres <40176339+Andres20-Utec@users.noreply.github.com>
2023-09-22 22:52:12 -0500

    fixed

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker run -d --rm --volume=/:/roots:ro --volume=/var/run:/var/run:rw --volume=/sys:/sys:ro --volume=/var/lib/docker:/var/lib/docker:ro -p 8080:8080 --privileged=true --name cadvisor google/cadvisor:latest
+docker run -d --rm --volume=/:/roots:ro --volume=/var/run:/var/run:rw --volume=/sys:/sys:ro --volume=/var/lib/docker:/var/lib/docker:ro -p 8080:8080 --privileged=true --name cadvisor gcr.io/cadvisor/cadvisor
 docker build -t spark_1.3_task:v1.0 .
 docker run -d -v $(pwd)/Data:/home/app/Data spark_1.3_task:v1.0
