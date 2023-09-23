a38d74bcfde1aec2bfac8f82774ed86ffdca710c
JohanTv <johan.tanta@utec.edu.pe>
2023-09-22 23:21:19 -0500

    fixed: start container again

--- App/WordCount.py
+++ App/WordCount.py
@@ -1,10 +1,12 @@
 from pyspark import SparkConf, SparkContext
+import os
 
 conf = SparkConf().setAppName("WordCount")
 sc = SparkContext(conf=conf)
 
 HOMEAPP_DATA = "/home/app/Data/"
 filename = "movies.csv"
+output = HOMEAPP_DATA + "output" + str(len(os.listdir(HOMEAPP_DATA)))
 
 lines = sc.textFile(HOMEAPP_DATA + filename)
 
@@ -16,6 +18,6 @@ sorted_word_count = word_count.sortBy(lambda x: x[1], ascending=False)
 
 sorted_word_count_single_partition = sorted_word_count.coalesce(1)
 
-sorted_word_count_single_partition.saveAsTextFile(HOMEAPP_DATA + "output")
+sorted_word_count_single_partition.saveAsTextFile(output)
 
 sc.stop()
